8fcaf0f4757fc31ff35eac6e289d48190bdbdec2
sAVItar02 <59026666+sAVItar02@users.noreply.github.com>
2020-05-11 01:40:17 +0530

    Level 1 created

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,34 +4,71 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
     Rigidbody rigidBody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        HandleRotation();
     }
 
-    private void ProcessInput()
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("WOKKAY");
+                break;
+            
+            default:
+                print("DED");
+                break;
+        }
+    }
+
+    private void Thrust()
+
     {
         if( Input.GetKey(KeyCode.Space) )
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if(!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
         }
+        else{
+            audioSource.Stop();
+        }
+    }
 
+    private void HandleRotation()
+    {
+        rigidBody.freezeRotation = true; //take manual control of rotation
+
+        float rotationThisframe = rcsThrust * Time.deltaTime;
+        
         if( Input.GetKey(KeyCode.A) )
         {
-            print("turning left");
+            transform.Rotate(Vector3.forward * rotationThisframe);
         }
         else if( Input.GetKey(KeyCode.D))
         {
-            print("turning right");
+            transform.Rotate(-Vector3.forward * rotationThisframe);
         }
-        
+
+        rigidBody.freezeRotation = false; //let the physics take control
     }
 }
